e4f94703ed2364035f39634b0219ebff2506ea72
Anne Enonkoski <anne.enonkoski@stud.pointcollege.fi>
2016-11-06 15:28:36 +0200

    AssetManagementWeb,AssetManagementWeb.Controllers.AssetController (kesken)

---
+++ AssetManagementWeb/AssetManagementWeb/Controllers/AssetController.cs
@@ -0,0 +1,112 @@
+﻿using AssetManagementWeb.Database;
+using AssetManagementWeb.Models;
+using AssetManagementWeb.Utilities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetManagementWeb.Controllers
+{
+    public class AssetController : Controller
+    {
+        // GET: Asset
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Asset/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Asset/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AssignLocation()
+        {
+            string json = Request.InputStream.ReadToEnd();
+            AssignLocationModel inputData = JsonConvert.DeserializeObject<AssignLocationModel>(json);
+
+            AssetLocationsDatabaseEntities = new AssetLocationsDatabaseEntities();
+
+            try
+            {
+                int locationId=(from l in  //tähän jäin
+            }
+            catch
+            {
+
+            }
+
+
+        }
+        // POST: Asset/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asset/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asset/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Asset/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
